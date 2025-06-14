5fd9430254b6bd45aed991ae953a9bdabca70111
LamcatUK <git@lamcat.co.uk>
2025-06-14 18:03:26 +0100

    pricing block

--- page-templates/blocks/lc_price_list.php
+++ page-templates/blocks/lc_price_list.php
@@ -9,16 +9,18 @@
 
 defined( 'ABSPATH' ) || exit;
 ?>
-<div class="price-list mx-auto">
-	<?php
-	while ( have_rows( 'prices' ) ) {
-		the_row();
-		?>
-	<div class="price-list__item">
-    	<span class="name"><?= esc_html( get_sub_field( 'item' ) ); ?></span>
-    	<span class="price"><?= esc_html( get_sub_field( 'price' ) ); ?></span>
-	</div>
+<div class="container">
+	<div class="price-list mx-auto">
 		<?php
-	}
-	?>
+		while ( have_rows( 'prices' ) ) {
+			the_row();
+			?>
+		<div class="price-list__item">
+			<span class="name"><?= esc_html( get_sub_field( 'item' ) ); ?></span>
+			<span class="price"><?= esc_html( get_sub_field( 'price' ) ); ?></span>
+		</div>
+			<?php
+		}
+		?>
+	</div>
 </div>
