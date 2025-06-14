c9f7a8ea0538c6fd6afacbd0c299432472d43732
LamcatUK <git@lamcat.co.uk>
2025-06-14 17:58:43 +0100

    price list

--- page-templates/blocks/lc_price_list.php
+++ page-templates/blocks/lc_price_list.php
@@ -1,16 +1,23 @@
 <?php
+/**
+ * Price List Block Template
+ *
+ * Displays a list of prices and items.
+ *
+ * @package lc-delphine2025
+ */
 
-
+defined( 'ABSPATH' ) || exit;
 ?>
-<div class="price-list">
+<div class="price-list mx-auto">
 	<?php
-	while (have_rows('prices')) {
+	while ( have_rows( 'prices' ) ) {
 		the_row();
 		?>
-  <div class="price-item">
-    <span class="name"><?= esc_html( get_sub_field( 'item' ) ); ?></span>
-    <span class="price"><?= esc_html( get_sub_field( 'price' ) ); ?></span>
-  </div>
+	<div class="price-list__item">
+    	<span class="name"><?= esc_html( get_sub_field( 'item' ) ); ?></span>
+    	<span class="price"><?= esc_html( get_sub_field( 'price' ) ); ?></span>
+	</div>
 		<?php
 	}
 	?>
